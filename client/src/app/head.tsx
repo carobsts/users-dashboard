12f47c4a27d2df7f5788323a575a63d46a9e492d
carobsts <bsts.carolina@gmail.com>
2025-04-12 10:48:49 -0500

    feat: remove fragments

--- client/src/app/head.tsx
+++ client/src/app/head.tsx
@@ -1,7 +1,3 @@
 export default function Head() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </>
-  );
+  return <meta name="viewport" content="width=device-width, initial-scale=1" />;
 }
